90aa1aa9504b7ee28ac6b54bd3fc2b7ffd13da7c
hks9211 <honeykrsingh16@gmail.com>
2020-01-21 15:34:58 +0530

    minor fixes

--- server/services/cpucheck.js
+++ server/services/cpucheck.js
@@ -2,43 +2,46 @@ const os = require('os');
 const cron = require('node-cron');
 
 cron.schedule('* * * * *', () => {
-    let cpuLoad = getCpuLoad();
-    if(cpuLoad > 70){
-        process.exit(1);
-    }
-});
-//cpus is all cores, we need the average of all the cores which 
-//will give us the cpu average
+    //cpus is all cores, we need the average of all the cores which 
+    //will give us the cpu average
 
-function cpuAverage() {
-    const cpus = os.cpus();
+    function cpuAverage() {
+        const cpus = os.cpus();
 
-    let idleMs = 0;
-    let totalMs = 0;
-    cpus.forEach((aCore) => {
-        for (type in aCore.times) {
-            totalMs += aCore.times[type];
+        let idleMs = 0;
+        let totalMs = 0;
+        cpus.forEach((aCore) => {
+            for (type in aCore.times) {
+                totalMs += aCore.times[type];
+            }
+            idleMs += aCore.times.idle;
+
+        });
+        return {
+            idle: idleMs / cpus.length,
+            total: totalMs / cpus.length
         }
-        idleMs += aCore.times.idle;
-
-    });
-    return {
-        idle: idleMs / cpus.length,
-        total: totalMs / cpus.length
     }
-}
 
-function getCpuLoad() {
 
     const start = cpuAverage();
+    let percentageCpu = 0;
     setTimeout(() => {
         const end = cpuAverage();
         const idleDifference = end.idle - start.idle;
         const totalDifference = end.total - start.total;
 
         //cal percentage cpu usuage
-        const percentageCpu = 100 - Math.floor(100 * idleDifference / totalDifference);
-        return percentageCpu;
+        percentageCpu = 100 - Math.floor(100 * idleDifference / totalDifference);
+        console.log(percentageCpu);
+        //PM2 or forever will help it  to start kindly install in your system to make it work
+        if (percentageCpu > 70)
+            process.exit(1);
+
     }, 100)
-}
+
+
+
+
+});
 
